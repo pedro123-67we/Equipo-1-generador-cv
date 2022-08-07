4730cf421fc724390252849d9015871605782456
isakk7 <74216113+isakk7@users.noreply.github.com>
2022-08-06 21:31:26 -0500

    feat: navbar has been added to the page

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -1,1 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
 
+
+ export const Inicio = () => {
+ return (
+    <div>
+    <div className="header">
+      <a href="#default" className="logo">
+        <img
+          src=""
+          alt="logo"
+        ></img>
+      </a>
+      <div className="header-right">
+      <Link to="/generador">Generador</Link> 
+      <Link to="/vistaCv">CV</Link> 
+       
+      </div>
+    </div>
+  
+  </div>
+    )
+ }
+export default Inicio
