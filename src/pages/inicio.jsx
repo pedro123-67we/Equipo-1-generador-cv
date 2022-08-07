bf4dc355323db6602a186754e8a7733d5319f079
isakk7 <74216113+isakk7@users.noreply.github.com>
2022-08-06 21:45:58 -0500

    feat: navbar has been added to the page

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -12,13 +12,12 @@ import {Link} from 'react-router-dom'
           alt="logo"
         ></img>
       </a>
-      
+
       <div className="header-right">
       <Link to="/generador">Generador</Link> 
       <Link to="/vistaCv">CV</Link> 
-    
-      </div>
 
+      </div>
     </div>
   
   </div>
