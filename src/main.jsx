8e28ecd727f5c7a807ab42571d741fe1c6c3d4f7
Lupitaglz <lupitaglzl84@gmail.com>
2022-08-08 16:57:25 -0500

    docs: the useTranslation report was implemented and the internationalization parameters were added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
+import global_es from "./traducciones/es/glonal.json";
+import global_en from "./traducciones/en/global.json";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+i18next.init({
+  interpolation:{ escapeValue: false},
+  lng: "es",
+  resources: {
+    es:{
+      global: global_es
+    },
+    en:{
+      global: global_en
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-    
+    <I18nextProvider i18n={i18next}>
+      <App />  
+    </I18nextProvider>   
   </React.StrictMode>
+);
 
-)
