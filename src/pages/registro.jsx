0769b5c20783fd871f46203782ad69547ae36aa3
Lupitaglz <lupitaglzl84@gmail.com>
2022-08-08 17:01:59 -0500

    docs: the import of useTranslation was implemented and buttons were added to change the language

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import '../App'
+import { useTranslation } from 'react-i18next'
 
-
-const Registro= ()=>(
+const Registro= ()=>{
+    const [t, i18n] = useTranslation("global")
+    return(
         <div className="registro">
             <form >
+                    <button onClick={() => i18n.changeLanguage("es")}>Esapañol</button>
+                    <button onClick={() => i18n.changeLanguage("en")}>Ingles</button>
+                    <br/> <br/>
                 <div className="container">
-                    <label htmlFor="">Nombre:</label>
+                    <label htmlFor="">{t("name.nom")}:</label>
                     <input type={'text'} />
-                    <label htmlFor="">Apellidos:</label>
+                    <label htmlFor="">{t("apelli.appe")}:</label>
                     <input type={'email'} />
-                    <label htmlFor="">Email:</label>
+                    <label htmlFor="">{t("email.email")}:</label>
                     <input type={'text'} />
-                    <label htmlFor="">Telefono:</label>
+                    <label htmlFor="">{t("cell.phone")}:</label>
                     <input type={'tel'} />
-                    <label htmlFor="">Password:</label>
+                    <label htmlFor="">{t("pass.pass")}:</label>
                     <input type="text" />
-                    <button>Save</button>
+                    <button>{t("save.save")}</button>
                 </div>
             </form>
         </div>
     )
-    
+}
+        
 
 export default Registro
