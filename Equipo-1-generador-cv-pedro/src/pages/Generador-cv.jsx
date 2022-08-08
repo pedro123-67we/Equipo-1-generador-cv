788ee085485992d4ef89fe59f82c4e30ceb893c7
rafael.ramirez <rafa.rami12.10@gmail.com>
2022-08-08 12:32:08 -0500

    Feat:a class was added to the div in the cv-generator, called label1

--- Equipo-1-generador-cv-pedro/src/pages/Generador-cv.jsx
+++ Equipo-1-generador-cv-pedro/src/pages/Generador-cv.jsx
@@ -1,35 +1,37 @@
 import React from 'react'
 
-
 const GeneradorCv= ()=>(
-    <div className="generador">
-        <form >
-            <div className="container-generador">
-                <label htmlFor="">Nombre:</label>
-                <input type={'text'} />
-
-                <label htmlFor="">Apellidos:</label>
-                <input type={'text'} />
-                
-                <label htmlFor="">Carrera:</label>
-                <input type={'text'}/>
-                <label htmlFor="">Email:</label>
-                <input type={'email'} />
-                <label htmlFor="">Experiencia:</label>
-                <input type={'text'}/>
-                <label htmlFor="">Educacion:</label>
-                <input type={'text'}/>
-                <label htmlFor="">Descripcion:</label>
-                <input type={'text'}/>
-                <label htmlFor="">Telefono:</label>
-                <input type={'tel'} />
-                <label htmlFor="">Redes:</label>
-                <input type={'text'}/>
-
-                <button>Save</button>
+    <div>
+    <form>
+        <div className="cont-generador">
+                <div className="label1">
+                    <label htmlFor="">Nombre:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Apellido:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Carrera:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Email:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Experiencia:</label>
+                    <input className="inpu1" type={'text'} />
+                </div>
+            
+            <div className="label1">
+                    <label htmlFor="">Telefono:</label>
+                    <input className="inpu1" type={'tel'} />
+                    <label htmlFor="">Eduacacion:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Descripcion:</label>
+                    <input className="inpu1" type={'text'} />
+                    <label htmlFor="">Redes:</label>
+                    <input className="inpu1" type={'text'} />  
             </div>
-        </form>
-    </div>
+            <button className="guardar">Save</button>           
+            </div>
+    </form>
+</div>
 )
 
 export default GeneradorCv
+
