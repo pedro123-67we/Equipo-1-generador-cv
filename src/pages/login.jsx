8fc8488873c89916cb76679785319b3cea3433ad
Lupitaglz <lupitaglzl84@gmail.com>
2022-08-08 17:01:27 -0500

    docs: the import of useTranslation was implemented and buttons were added to change the language

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import { FaUserCircle } from 'react-icons/fa';
 import {Link} from 'react-router-dom'
 import '../App'
-
+import { useTranslation } from 'react-i18next';
 
 const Login = () => {
+    const [t, i18n] = useTranslation("global")
     return (
         <div>
             <form>
                 <div className="login">
                     <div className='titulo'>
+                    <button onClick={() => i18n.changeLanguage("es")}>Esapañol</button>
+                    <button onClick={() => i18n.changeLanguage("en")}>Ingles</button>
+                    <br/> <br/>
                         <FaUserCircle className='user-icon'/>
                         <h1>Login</h1>
                     </div>
-                        <label htmlFor="*">Email:</label>
-                        <input type={'email'} placeholder="Escribe tu email"/>
-                        <label htmlFor="*">Password:</label>
-                        <input type={'password'}placeholder="Escribe tu contraseña" />
-                        <button className='ingresar'>INGRESAR</button>
-                        <Link to="/Registro">Registrate</Link>      
+                        <label htmlFor="*">{t("email.email")}:</label>
+                        <input type={'email'} placeholder={t("wirtee.ema")}/>
+                        <label htmlFor="*">{t("pass.pass")}:</label>
+                        <input type={'password'}placeholder={t("write.con")} />
+                        <button className='ingresar'>{t("ingre.ingre")}</button>
+                        <Link to="/Registro">{t("reg.reg")}</Link>      
                     </div>
             </form>
     </div>
